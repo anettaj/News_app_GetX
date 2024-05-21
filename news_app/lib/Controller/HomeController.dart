b19cdeb84d7832a22996b0f8e226946fab265a6c
anettaj <anuanet85@gmail.com>
2024-05-21 22:57:19 +0530

    Controller added

---
+++ news_app/lib/Controller/HomeController.dart
@@ -0,0 +1,29 @@
+import 'package:get/get.dart';
+import 'package:news_app/Model/NewsModel.dart';
+import 'package:news_app/Services/NewsServices.dart';
+
+class HomeController extends GetxController {
+  @override
+  void onInit() {
+    getNews();
+    super.onInit();
+  }
+
+  NewsApi? newsApi;
+  void getNews()async{
+    newsApi= await NewsServices().getNewsdata();
+  }
+
+  
+  void onReady() {
+    super.onReady();
+
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+
+}
